c519bd7fd7f398a07246e6bec72880a9bd24a178
Paulo Cidro <paulocidro@gmail.com>
2025-10-28 08:49:23 -0300

    adcionando os cards

--- script.js
+++ script.js
@@ -20,17 +20,58 @@ async function fetchMovies() {
   renderMovies(data.results);
 }
 
+function createMovieCard(movie) {
+  const movieCard = document.createElement("li");
+  movieCard.classList.add("movie");
+
+  movieCard.appendChild(createMovieImage(movie));
+  movieCard.appendChild(createMovieTitle(movie));
+  movieCard.appendChild(createMovieRate(movie));
+  movieCard.appendChild(createAddButton());
+  return movieCard;
+}
+
+function createMovieImage(movie) {
+  const movieImg = document.createElement("img");
+  movieImg.src = movie.poster_path
+    ? `https://image.tmdb.org/t/p/w300${movie.poster_path}`
+    : "/img/NoImage.png";
+  return movieImg;
+}
+
+function createMovieTitle(movie) {
+  const movieTitle = document.createElement("h1");
+  movieTitle.classList.add("movie-title");
+  movieTitle.textContent = movie.title;
+  return movieTitle;
+}
+
+function createMovieRate(movie) {
+  const starRate = document.createElement("div");
+  starRate.classList.add("star-rate");
+  const rate = document.createElement("span");
+  rate.textContent = movie.vote_average.toFixed(1);
+  const imgRate = document.createElement("img");
+  imgRate.src = "/img/star.svg";
+  starRate.appendChild(imgRate);
+  starRate.appendChild(rate);
+  return starRate;
+}
+
+function createAddButton() {
+  const button = document.createElement("button");
+  button.classList.add("btn-assistir");
+  const imgButton = document.createElement("img");
+  imgButton.src = "/img/plus.svg";
+  button.appendChild(imgButton);
+  button.textContent = "Add To List";
+  return button;
+}
+
 function renderMovies(movies) {
   moviesList.innerHTML = "";
   movies.forEach((movie) => {
-    const movieCard = document.createElement("li");
-    movieCard.classList.add("movie");
-    const movieName = document.createElement("h1");
-    movieName.textContent = movie.title;
-    const movieImg = document.createElement("img");
-    movieImg.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-    movieCard.appendChild(movieName);
-    movieCard.appendChild(movieImg);
-    moviesList.appendChild(movieCard);
+    const card = createMovieCard(movie);
+    moviesList.appendChild(card);
   });
 }
